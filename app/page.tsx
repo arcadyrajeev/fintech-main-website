170b1ca6c2f0a253dcb3662bb0e3c492ebe39348
arcadyrajeev <bitmasteratj@gmail.com>
2026-05-29 00:38:43 +0530

    bsdfs

--- app/page.tsx
+++ app/page.tsx
@@ -121,8 +121,6 @@ export default function Home() {
         <WorkflowSection />
         <CommonProblemsSection />
 
-        <POVSection />
-
         <FinalSection />
         <BlogSection />
         <FAQSection />
